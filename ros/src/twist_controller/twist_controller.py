ecbab5329fefe50a9c8fd5003707573b7628ddb7
bknarf <b.knarf@outlook.com>
2020-06-10 13:31:47 +0200

    changed imports

--- ros/src/twist_controller/twist_controller.py
+++ ros/src/twist_controller/twist_controller.py
@@ -1,9 +1,9 @@
 
 GAS_DENSITY = 2.858
 ONE_MPH = 0.44704
-from .yaw_controller import YawController
-from .pid import PID
-from .lowpass import LowPassFilter
+from yaw_controller import YawController
+from pid import PID
+from lowpass import LowPassFilter
 import rospy
 
 
