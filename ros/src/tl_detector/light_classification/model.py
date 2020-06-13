3d091cccc20855f7cdfb5fd39b4f5a5c9834dcd9
bknarf <b.knarf@outlook.com>
2020-06-13 12:57:36 +0200

    layer changes

--- ros/src/tl_detector/light_classification/model.py
+++ ros/src/tl_detector/light_classification/model.py
@@ -88,10 +88,11 @@ if __name__ == "__main__":
     model.add(Lambda(lambda x: (x / 255.0) - 0.5))
     model.add(Conv2D(filters=6, kernel_size=(55*3, 55), activation='relu'))
     model.add(Dropout(0.2))
-    model.add(Conv2D(filters=16, kernel_size=(20, 10), activation='relu'))
+    model.add(Conv2D(filters=12, kernel_size=(20, 10), activation='relu'))
     model.add(AveragePooling2D())
-    model.add(Conv2D(filters=16, kernel_size=(12, 4), activation='relu'))
-    model.add(Conv2D(filters=16, kernel_size=(3, 1), activation='relu'))
+    model.add(Conv2D(filters=8, kernel_size=(12, 4), activation='relu'))
+    model.add(AveragePooling2D())
+    model.add(Conv2D(filters=4, kernel_size=(6, 2), activation='relu'))
 
     model.add(Flatten())
 
