ea356e7fd5f5e35bdf35676b0eb8164b210c1492
bknarf <b.knarf@outlook.com>
2020-06-13 14:27:58 +0200

    layer changes

--- ros/src/tl_detector/light_classification/model.py
+++ ros/src/tl_detector/light_classification/model.py
@@ -79,25 +79,40 @@ if __name__ == "__main__":
 
 
 
-    #Start creating the model
+
     model = Sequential()
 
-    #crop to interesting area of image
-    #normalize the data
-    model.add(AveragePooling2D(input_shape=(600, 800, 3)))
-    model.add(Lambda(lambda x: (x / 255.0) - 0.5))
-    model.add(Conv2D(kernel_size = (3*15,15), filters = 16, padding='same', activation='relu'))
-    #model.add(AveragePooling2D())
-    model.add(Dropout(0.2))
-    model.add(Conv2D(kernel_size = (3*5,5), filters = 32, padding='same', activation='relu'))
-    model.add(AveragePooling2D())
-    model.add(Conv2D(kernel_size = (3*2,2), filters = 16, padding='same', activation='relu'))
-    model.add(AveragePooling2D())
-    model.add(Dropout(0.2))
+    model.add(Lambda(lambda x: (x / 255.0) - 0.5),input_shape=(600, 800, 3))
+    # Layer 1
+    # Conv Layer 1
+    model.add(Conv2D(filters=6,
+                     kernel_size=5,
+                     strides=1,
+                     activation='relu',
+                     input_shape=(600, 800, 3)))
+    # Pooling layer 1
+    model.add(MaxPooling2D(pool_size=2, strides=2))
+    # Layer 2
+    # Conv Layer 2
+    model.add(Conv2D(filters=16,
+                     kernel_size=5,
+                     strides=1,
+                     activation='relu',
+                     input_shape=(14, 14, 6)))
+    # Pooling Layer 2
+    model.add(MaxPooling2D(pool_size=2, strides=2))
+    # Flatten
     model.add(Flatten())
-    model.add(Dense(220, activation='relu'))
-    model.add(Dense(11, activation='relu'))
-    model.add(Dense(units=3, activation='softmax'))
+    # Layer 3
+    # Fully connected layer 1
+    model.add(Dense(units=120, activation='relu'))
+    # Layer 4
+    # Fully connected layer 2
+    model.add(Dense(units=84, activation='relu'))
+    # Layer 5
+    # Output Layer
+    model.add(Dense(units=10, activation='softmax'))
+    model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
 
     print(model.summary())
 
