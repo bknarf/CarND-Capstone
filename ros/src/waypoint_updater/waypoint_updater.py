f2c1b0d8aa2a65d93502132f0b5a00e287c22dda
bknarf <b.knarf@outlook.com>
2020-06-14 15:24:23 +0200

    drive

--- ros/src/waypoint_updater/waypoint_updater.py
+++ ros/src/waypoint_updater/waypoint_updater.py
@@ -90,9 +90,9 @@ class WaypointUpdater(object):
     def publish_waypoints(self,idx):
         idx = max(0, idx)
         last_idx = min(idx + LOOKAHEAD_WPS,len(self.base_waypoints.waypoints)-1)
-        rospy.logwarn("waypoint_updater:  next waypoint index = {0}".format(idx))
-        rospy.logwarn("waypoint_updater:  len(base_waypoints.waypoints):{0} wp1:{1} wp2:{2}".format(
-            len(self.base_waypoints.waypoints),idx, last_idx))
+        #rospy.logwarn("waypoint_updater:  next waypoint index = {0}".format(idx))
+        #rospy.logwarn("waypoint_updater:  len(base_waypoints.waypoints):{0} wp1:{1} wp2:{2}".format(
+        #    len(self.base_waypoints.waypoints),idx, last_idx))
         start_dist = self.distance(self.base_waypoints.waypoints, idx, last_idx)
         x = [ start_dist +1, start_dist ]
         current_velocity = self.current_velocity
