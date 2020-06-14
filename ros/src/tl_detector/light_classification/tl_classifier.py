dac5e465e056118e39695b2d28a51359021f9d01
bknarf <b.knarf@outlook.com>
2020-06-14 10:23:52 +0200

    default graph for tensorflow

--- ros/src/tl_detector/light_classification/tl_classifier.py
+++ ros/src/tl_detector/light_classification/tl_classifier.py
@@ -2,12 +2,13 @@ from styx_msgs.msg import TrafficLight
 from keras.models import load_model
 import numpy as np
 import os
+import tensorflow as tf
 
 class TLClassifier(object):
     def __init__(self):
         self.modelpath = os.path.join(os.path.split(os.path.abspath(__file__))[0],"model_96.h5")
         self.model = load_model(self.modelpath)
-        pass
+        self.graph = tf.get_default_graph()
 
     def get_classification(self, image):
         """Determines the color of the traffic light in the image
@@ -19,7 +20,8 @@ class TLClassifier(object):
             int: ID of traffic light color (specified in styx_msgs/TrafficLight)
 
         """
-        image_array = np.asarray(image)
-        image_array = image_array[None, :, :, :]
-        prediction = int(self.model.predict(image_array, batch_size=1))
-        return prediction
+        with self.graph.as_default():
+            image_array = np.asarray(image)
+            image_array = image_array[None, :, :, :]
+            prediction = int(self.model.predict(image_array, batch_size=1))
+            return prediction
